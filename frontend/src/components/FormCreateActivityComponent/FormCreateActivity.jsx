4d49d4ed8bfe5728ff435b3a247a3c6553b95fe2
antonio.moreno.dev <antonio.moreno.dev@gmail.com>
2022-04-08 09:03:14 -0500

    Add ForntEnd FormCreateActivityComponent

---
+++ frontend/src/components/FormCreateActivityComponent/FormCreateActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormCreateActivityComponent = () => {
+    return (
+        <div>
+            <h1>Form Create Activity Component</h1>
+        </div>
+    )
+}
+
+export default FormCreateActivityComponent;
